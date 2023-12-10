5085336fc2fada4e054ff528ce69aa55db104d8a
vadosiuoso <trezstyle@gmail.com>
2023-12-10 19:34:32 +0100

    init commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,90 +1,99 @@
 import java.util.Scanner;
 
+
+
 public class App {
-
+    private static final char[] BOX = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    private static final int[][] WINNING_COMBINATIONS = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8},  // Rows
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},  // Columns
+            {0, 4, 8}, {2, 4, 6}               // Diagonals
+    };
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        byte input;
-        byte rand;
-        byte i;
-        boolean boxAvailable = false;
-        byte winner = 0;
-        char box[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        System.out.println("Enter box number to select. Enjoy!\n");
-
+        startGame();
+    }
+    public static void startGame() {
+        logger("Enter box number to select. Enjoy!\n");
+        view();
         boolean boxEmpty = false;
         while (true) {
-            System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
             if(!boxEmpty){
-                for(i = 0; i < 9; i++)
-                    box[i] = ' ';
+                for(byte i = 0; i < 9; i++)
+                    BOX[i] = ' ';
                 boxEmpty = true;
             }
+            logger("You are next!\n\n");
 
-            if(winner == 1){
-                System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
+            customerMove();
+            if(checkWinner('X'))
                 break;
-            } else if(winner == 2){
-                System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
+            if(!isBoxAvailable())
                 break;
-            } else if(winner == 3){
-                System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+            logger("The computer makes the next move!");
+            botMove();
+            if(checkWinner('0'))
+                break;
+        }
+    }
+    private static void botMove(){
+        byte rand;
+        while (true) {
+            rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
+            if (BOX[rand - 1] != 'X' && BOX[rand - 1] != 'O') {
+                BOX[rand - 1] = 'O';
+                view();
                 break;
             }
-
-            while (true) {
-                input = scan.nextByte();
-                if (input > 0 && input < 10) {
-                    if (box[input - 1] == 'X' || box[input - 1] == 'O')
-                        System.out.println("That one is already in use. Enter another.");
-                    else {
-                        box[input - 1] = 'X';
-                        break;
-                    }
+        }
+    }
+    private static void customerMove() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            byte input;
+            input = scan.nextByte();
+            if (isValidInput(input)) {
+                if (BOX[input - 1] == 'X' || BOX[input - 1] == 'O')
+                    logger("That one is already in use. Enter another.");
+                else {
+                    BOX[input - 1] = 'X';
+                    view();
+                    return;
                 }
-                else
-                    System.out.println("Invalid input. Enter again.");
+            } else
+                logger("Invalid input. Enter again.");
+        }
+    }
+    private static boolean isValidInput(byte input){
+        return input > 0 && input < 10;
+    }
+    private static boolean isBoxAvailable(){
+        for (byte i = 0; i < 9; i++) {
+            if (BOX[i] != 'X' && BOX[i] != 'O') {
+                return true;
             }
-
-            if((box[0]=='X' && box[1]=='X' && box[2]=='X') || (box[3]=='X' && box[4]=='X' && box[5]=='X') || (box[6]=='X' && box[7]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[3]=='X' && box[6]=='X') || (box[1]=='X' && box[4]=='X' && box[7]=='X') || (box[2]=='X' && box[5]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[4]=='X' && box[8]=='X') || (box[2]=='X' && box[4]=='X' && box[6]=='X')){
-                   winner = 1;
-                   continue;
-            }
-
-            boxAvailable = false;
-            for(i=0; i<9; i++){
-                if(box[i] != 'X' && box[i] != 'O'){
-                    boxAvailable = true;
-                    break;
-                }
-            }
-
-            if(boxAvailable == false){
-                winner = 3;
-                continue;
-            }
-
-            while (true) {
-                rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
-                if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
-                    box[rand - 1] = 'O';
-                    break;
-                }
-            }
-
-            if((box[0]=='O' && box[1]=='O' && box[2]=='O') || (box[3]=='O' && box[4]=='O' && box[5]=='O') || (box[6]=='O' && box[7]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[3]=='O' && box[6]=='O') || (box[1]=='O' && box[4]=='O' && box[7]=='O') || (box[2]=='O' && box[5]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[4]=='O' && box[8]=='O') || (box[2]=='O' && box[4]=='O' && box[6]=='O')){
-                winner = 2;
-                continue;
+        }
+        logger("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+        return false;
+    }
+    private static boolean checkWinner(char symbol) {
+        for (int[] combination : WINNING_COMBINATIONS) {
+            if (BOX[combination[0]] == symbol && BOX[combination[1]] == symbol && BOX[combination[2]] == symbol) {
+                logger(symbol == 'X' ? "You won the game!\n" : "You lost the game!\n");
+                logger("Created by Shreyas Saha. Thanks for playing!");
+                return true;
             }
         }
 
+        return false;
+    }
+    private static void view() {
+        logger("\n\n " + BOX[0] + " | " + BOX[1] + " | " + BOX[2] + " ");
+        logger("-----------");
+        logger(" " + BOX[3] + " | " + BOX[4] + " | " + BOX[5] + " ");
+        logger("-----------");
+        logger(" " + BOX[6] + " | " + BOX[7] + " | " + BOX[8] + " \n");
+    }
+    private static void logger(String text){
+        System.out.println(text);
     }
 }
